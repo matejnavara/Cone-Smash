42b762450adfbe294972eaeb78d90666c57290a2
Matej Navara <mn98@uni.brighton.ac.uk>
2016-04-12 23:45:03 +0100

    Added new levels
    Created scene and script files for level 3 and 4. Still need to add
    content.

---
+++ ConeSmash/Assets/Scripts/level3.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level3 : Level {
+
+    public level3()
+    {
+        levelName = "level3";
+		levelIndex = 2;
+		levelStartTime = 30f;
+
+    }
+		
+	
+}
