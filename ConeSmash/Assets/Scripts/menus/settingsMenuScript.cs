301052ae40ee224045f56d5c0b953e680bc98e63
Matej Navara <mn98@uni.brighton.ac.uk>
2016-04-25 23:19:40 +0100

    Implementing music/sfx states (In Progress)
    Remembers player's choice of music/sfx mute state. Not yet implemented!

--- ConeSmash/Assets/Scripts/menus/settingsMenuScript.cs
+++ ConeSmash/Assets/Scripts/menus/settingsMenuScript.cs
@@ -6,6 +6,8 @@ public class settingsMenuScript : MonoBehaviour {
 
     private Button classicControl;
     private Button tiltControl;
+    private Toggle musicToggle;
+    private Toggle sfxToggle;
 
     public Canvas levelMenu;
 
@@ -16,9 +18,47 @@ public class settingsMenuScript : MonoBehaviour {
         classicControl = GameObject.Find("SettingsMenu/ClassicControl").GetComponent<Button>();
         tiltControl = GameObject.Find("SettingsMenu/TiltControl").GetComponent<Button>();
 
+        musicToggle = GameObject.Find("SettingsMenu/MusicToggle").GetComponent<Toggle>();
+        sfxToggle = GameObject.Find("SettingsMenu/SfxToggle").GetComponent<Toggle>();
+
         if (PlayerPrefs.HasKey("Control")) { controlChoice(PlayerPrefs.GetString("Control")); } else { controlChoice("classic"); }
+        if(PlayerPrefs.HasKey("Music") && PlayerPrefs.GetString("Music") == "off") { musicToggle.isOn = false; } else { musicToggle.isOn = true; }
     }
-	
+
+
+    //FIX TOGGLES
+    public void toggleMusic()
+    {
+        if (musicToggle.isOn == true)
+        {
+            musicToggle.isOn = false;
+            PlayerPrefs.SetString("Music", "off");
+            print("MUSIC OFF");
+        }else if (musicToggle.isOn == false)
+        {
+            musicToggle.isOn = true;
+            PlayerPrefs.SetString("Music", "on");
+            print("MUSIC ON");
+        }
+    }
+
+    public void toggleSfx()
+    {
+        if (sfxToggle.isOn)
+        {
+            sfxToggle.isOn = false;
+            PlayerPrefs.SetString("Sfx", "off");
+            print("SFX OFF");
+        } else if (!sfxToggle.isOn)
+        {
+            sfxToggle.isOn = true;
+            PlayerPrefs.SetString("Sfx", "on");
+            print("SFX ON");
+        }
+    }
+
+
+
     void controlChoice(string control)
     {
         if(control == "classic") {
