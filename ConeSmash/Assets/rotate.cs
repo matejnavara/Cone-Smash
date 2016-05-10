78850c99fdd0b313831e40a55e91cc025b3ca2f7
Matej Navara <matej.navara@gmail.com>
2016-05-10 22:43:00 +0100

    Setting up animated gifs for shop front
    Photo shoot scene with each ball to rotate for animated gifs. These are
    to be used in the shop front.

---
+++ ConeSmash/Assets/rotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+
+        startTime = Time.deltaTime;
+        Debug.Log("Start time: " + startTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAroundLocal(Vector3.up, Time.deltaTime);
+        
+        if(transform.rotation.y > 350)
+        {
+            float endTime = Time.deltaTime - startTime;
+            Debug.Log("360 time: " + endTime);
+        }
+
+    }
+}
