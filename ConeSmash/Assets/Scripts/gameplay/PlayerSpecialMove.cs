e00983c7a1b8e05a32af764bc72a50db49f4af4d
Matej Navara <matej.navara@gmail.com>
2017-01-24 14:08:45 +0000

    Added individual special moves, fixed scaling
    Signed-off-by: Matej Navara <matej.navara@gmail.com>

--- ConeSmash/Assets/Scripts/gameplay/PlayerSpecialMove.cs
+++ ConeSmash/Assets/Scripts/gameplay/PlayerSpecialMove.cs
@@ -4,26 +4,64 @@ using UnityEngine.UI;
 
 public class PlayerSpecialMove : MonoBehaviour {
 
-    bool triggered;
-    float force = 50f;
-    public float rechargeCounter = 100f;
-    public float recharchRate = 0.2f;
+    public GameObject moveBtn;
+    public GameObject rechargeImage;
 
-    public GameObject recharge;
+    private Image image;
+    private Text text;
 
-    Rigidbody player;
-    RectTransform rechargeRect;
-    public Image rechargeImage;
+    private Vector3 force;
+    private float rechargeCounter;
+    private float recharchRate;
+    private Rigidbody player;
+    private bool triggered;
 
     // Use this for initialization
     void Start () {
 
         triggered = false;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
-        rechargeRect = recharge.GetComponent<RectTransform>();
-        rechargeImage = recharge.GetComponentInChildren<Image>();
+        image = rechargeImage.GetComponentInChildren<Image>();
+        text = moveBtn.GetComponentInChildren<Text>();
+        rechargeCounter = 100f;
+        selectMove();
 	
 	}
+
+    void selectMove()
+    {
+        int choice;
+
+        if (PlayerPrefs.HasKey("selectedBall"))
+        {
+            choice = PlayerPrefs.GetInt("selectedBall");
+        }
+        else {
+            choice = 1;
+        }
+
+        switch (choice)
+        {
+            case 1:
+                force = transform.up * 50f;
+                recharchRate = 0.3f;
+                text.text = "S H O O T !";
+                break;
+            case 2:
+                force = transform.up * 30f;
+                recharchRate = 0.5f;
+                text.text = "B O U N C E !";
+                break;
+            case 3:
+                force = transform.forward * 200f;
+                recharchRate = 0.2f;
+                text.text = "B O W L !";
+                break;
+            default:
+                print("Error case at selectMove");
+                break;
+        }
+    }
 
     public void triggerMove()
     {
@@ -38,7 +76,7 @@ public class PlayerSpecialMove : MonoBehaviour {
     {
         if (triggered)
         {
-            player.AddForce(transform.up * force, ForceMode.Impulse);
+            player.AddForce(force, ForceMode.Impulse);
             triggered = false;
             rechargeCounter = 0;
         }
@@ -46,16 +84,13 @@ public class PlayerSpecialMove : MonoBehaviour {
         if(rechargeCounter < 100f)
         {
             rechargeCounter += recharchRate;
-            rechargeImage.color = new Color(1, 0, 0, 1);
-            recharge.transform.localScale = new Vector3(1, rechargeCounter / 100f, 1);
+            rechargeImage.transform.localScale = new Vector3(1, rechargeCounter / 100f, 1);
+            image.color = new Color(1, 0, 0, 1);
         }
         else
         {
-            rechargeImage.color = new Color(0, 1, 0, 1);
+            image.color = new Color(0, 1, 0, 1);
         }
-
-        
-
     }
 	
 	
