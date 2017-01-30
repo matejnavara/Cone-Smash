e7eedbe724b4bc7185422155937d53392ba927d1
Matej Navara <matej.navara@gmail.com>
2017-01-30 23:42:33 +0000

    Added explosions with particle effects. BOOM!
    Signed-off-by: Matej Navara <matej.navara@gmail.com>

---
+++ ConeSmash/Assets/TntScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TntScript : MonoBehaviour {
+
+    public float radius;
+    public float power;
+    public float lift;
+    public GameObject tnt;
+    private bool exploded;
+    private Vector3 explosionPos;
+    private ParticleSystem explosion;
+
+
+	void Start () {
+
+        explosion = gameObject.GetComponent<ParticleSystem>();
+        exploded = false;
+        explosionPos = transform.position;
+        radius = 10.0f;
+        power = 5.0f;
+        lift = 3.0f;
+        	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && !exploded)
+        {
+            print("BOOM!");
+            explosion.Play();
+            tnt.SetActive(false);
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        exploded = true;
+        foreach (Collider hit in colliders)
+        { 
+            Rigidbody rb = hit.GetComponentInParent<Rigidbody>();
+            if(rb != null) {
+                print("Exploding..." + hit.name);
+                rb.AddExplosionForce(power, explosionPos, radius, lift, ForceMode.Impulse);
+            }
+        }  
+    }
+
+}
